574196b96ba44c5b4d7906824bff6ee3f54feb9f
Harry Mills <harry.e.mills@gmail.com>
2019-03-27 21:55:23 +0000

    Laying out GASEN method in comments

--- GASEN.py
+++ GASEN.py
@@ -1,4 +1,24 @@
+# Genetic Algorithm Selection of Ensemble Networks
+#
+# This script uses a genetic algorithm to selection the optimum
+# weighting of networks in an ensemble. The algorithm starts with random
+# weights and then starts to minimise the selected value in the
+# function ensemble_fitness.
+
 import tensorflow
 import keras
 
-# testing commit
+UPPER_BOUND = 1
+LOWER_BOUND = 0
+
+# Retrieve network objects from networks folder
+
+# Create objective function
+
+# Set Genetic Algorithm parameters
+
+# Run Genetic Algorithm to optimise objective function
+
+# Return weights
+
+# Display optimised network ensemble accuracy details
